612a53d650642268abe56d10dee2ef712d344f82
Neeraj Verma <neerajverma101@gmail.com>
2023-07-03 17:49:47 +0530

    node app (#4)

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const config = require("./config");
+const { PageNotFound, BadReq, ReqError } = require('./middleware/errors');
+const dbConnect = require('./config/dbConnect');
+const secrets = require('./config/aws-env');
+env.config();
+//dataBase connection
+dbConnect();
+
+if (process.env.NODE_ENV !== 'production') {
+    env.config()
+}
+
+app.get('/', (req, res) => {
+    res.send(req.httpVersion)
+});
+
+//error and errorHandler section
+app.all(ReqError);
+app.all(BadReq);
+app.all("*", PageNotFound)
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error found"
+    res.status(err.statusCode).json({
+        staus: err.statusCode,
+        message: err.message,
+    })
+})
+
+console.log(secrets)
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started on ${PORT}`));
