63545666819bfccdb2098e196ed0656360f71a7f
arpit9839 <arpit4499@gmail.com>
2023-09-15 00:07:17 +0530

    forgot password

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, appointment, fetchAppointment, addAddress } = require('../controllers/userController');
+const { register, login, appointment, fetchAppointment, addAddress, updatePassword, sendEmail } = require('../controllers/userController');
 const { protect } = require("../middleware/authMiddleware")
 const router = express.Router();
 
@@ -10,7 +10,8 @@ router.route('/appointment').post(protect, appointment)
 
 router.route('/fetchappointment').get(protect, fetchAppointment)
 router.route('/addAddress').post(protect, addAddress)
-
+router.route('/forgotpassword').post(updatePassword)
+router.route("/sendEmail").post(sendEmail);
 module.exports = router
 
 
