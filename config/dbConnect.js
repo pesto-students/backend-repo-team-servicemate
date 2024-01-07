93481754e4c679bc847c401a18b140d7fdd9f059
Neeraj Verma <neerajverma101@gmail.com>
2024-01-07 14:31:15 +0530

    db config changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const env = require('dotenv');
 
 env.config();
 const dbConnect = async () => {
-  const mongoUri = process.env.MONGO_URI;
+  const mongoUri = process.env.MONGO_URI || 'mongodb+srv://arpit4499:XNFtNNVbyD3Ecini@cluster0.mt4a4fw.mongodb.net/?retryWrites=true&w=majority';
   console.log(mongoUri);
   if (!mongoUri) {
     console.log('Could not connect to MongoDB');
